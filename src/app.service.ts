5887f16f30bb00892d9d1401ae7ae824da640f90
Marek Szczygieł <mszczygiel.poczta@gmail.com>
2020-03-07 22:32:10 +0100

    feature(logger): extend build-in logger by adding Winston

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,14 @@
 import { Injectable } from '@nestjs/common';
+import { AppLogger } from './logger.service';
 
 @Injectable()
 export class AppService {
+
+    constructor(private readonly logger: AppLogger) {
+        this.logger.setContext('app.service.ts');
+    }
+
     getHello(): string {
-        return 'Hello World REST API!';
+        return 'Hello World!!!';
     }
 }
